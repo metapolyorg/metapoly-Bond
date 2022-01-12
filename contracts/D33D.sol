b3268315790f920f1aea27e456dec7561aea4134
seshanthS <>
2022-01-12 11:31:27 +0530

    Updated D33D.sol

--- contracts/D33D.sol
+++ contracts/D33D.sol
@@ -1,68 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
 
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-pragma solidity 0.7.6;
-
-
-import "../libs/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
-import "../libs/contracts-upgradeable/token/ERC20/ERC20BurnableUpgradeable.sol";
-import "../libs/contracts-upgradeable/token/ERC20/ERC20PausableUpgradeable.sol";
-import "../libs/contracts-upgradeable/token/ERC20/ERC20SnapshotUpgradeable.sol";
-import "../libs/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "../libs/contracts-upgradeable/utils/AddressUpgradeable.sol";
-import "../libs/contracts-upgradeable/math/SafeMathUpgradeable.sol";
-import "../libs/contracts-upgradeable/proxy/Initializable.sol";
-
-
-contract D33DImplementation is Initializable, ERC20BurnableUpgradeable, ERC20PausableUpgradeable, ERC20SnapshotUpgradeable, OwnableUpgradeable {
-    using AddressUpgradeable for address;
-    using SafeMathUpgradeable for uint256;
-
-
-    /// A checkpoint for marking number of votes from a given block
-    struct Checkpoint {
-        uint32 fromBlock;
-        uint256 votes;
-    }
-
-
-    /// The EIP-712 typehash for the contract's domain
-    bytes32 public constant DOMAIN_TYPEHASH = keccak256("EIP712Domain(string name,uint256 chainId,address verifyingContract)");
-
-    /// The EIP-712 typehash for the delegation struct used by the contract
-    bytes32 public constant DELEGATION_TYPEHASH = keccak256("Delegation(address delegatee,uint256 nonce,uint256 expiry)");
-
-    // A record of each accounts delegate
-    mapping (address => address) internal _delegates;
-
-    /// A record of votes checkpoints for each account, by index
-    mapping (address => mapping (uint32 => Checkpoint)) public checkpoints;
-
-    /// The number of checkpoints for each account
-    mapping (address => uint32) public numCheckpoints;
-
-    /// A record of states for signing / validating signatures
-    mapping (address => uint) public nonces;
-
+contract D33DImplementation is Initializable, ERC20BurnableUpgradeable, OwnableUpgradeable {
+    
     address public treasury;
-    /// An event thats emitted when an account changes its delegate
-    event DelegateChanged(address indexed delegator, address indexed fromDelegate, address indexed toDelegate);
+    bool public feeOn;
+    uint public taxPerc; //300 for 3 % (2 decimals)
 
-    /// An event thats emitted when a delegate account's vote balance changes
-    event DelegateVotesChanged(address indexed delegate, uint previousBalance, uint newBalance);
+    mapping(address => bool) isDex;
+
+    mapping (address => uint256) private _balances;
+    uint256 private _totalSupply;
+
+    bool public isSenderTaxed; 
+    bool public isReceiverTaxed;
+    address public taxReceiver;
 
     event TreasuryUpdated(address newTreasury);
-
+    event UpdateDex(address dex,bool status);
+    event UpdateTaxReceiver(address receiver);
+    event TaxPercUpdated(uint _newPerc);
+    
     modifier onlyTreasury {
         require(msg.sender == treasury, "Only Treasury");
         _;
     }
 
-
     function initialize(string memory _name, string memory _symbol) external initializer {
         __ERC20_init(_name, _symbol);
         __ERC20Burnable_init();
-        __ERC20Pausable_init();
-        __ERC20Snapshot_init();
         __Ownable_init();
     }
 
@@ -71,359 +42,107 @@ contract D33DImplementation is Initializable, ERC20BurnableUpgradeable, ERC20Pau
         emit TreasuryUpdated(treasury_);
     }
 
-    /// @notice Creates `_amount` token to `_to`. Must only be called by the owner
-    function mint(address _to, uint256 _amount) public onlyTreasury whenNotPaused {
-        _mint(_to, _amount);
-        _moveDelegates(address(0), _delegates[_to], _amount);
+    function settaxPerc(uint _taxPerc) external onlyOwner {
+        taxPerc = _taxPerc;
+        emit TaxPercUpdated(_taxPerc);
     }
 
-    /**
-     * @dev Destroys `amount` tokens from the caller.
-     *
-     * See {ERC20-_burn}.
-     */
-    function burn(uint256 amount) public virtual override whenNotPaused {
-        _burn(_msgSender(), amount);
-        _moveDelegates(_delegates[_msgSender()], address(0), amount);
+    function updateTaxReceiver(address _receiver) external onlyOwner{
+        taxReceiver = _receiver;
+        emit UpdateTaxReceiver(_receiver);
     }
 
-    /**
-     * @dev Destroys `amount` tokens from `account`, deducting from the caller's
-     * allowance.
-     *
-     * See {ERC20-_burn} and {ERC20-allowance}.
-     *
-     * Requirements:
-     *
-     * - the caller must have allowance for ``accounts``'s tokens of at least
-     * `amount`.
-     */
-    function burnFrom(address account, uint256 amount) public virtual override whenNotPaused {
-        uint256 decreasedAllowance = allowance(account, _msgSender()).sub(amount, "ERC20: burn amount exceeds allowance");
-
-        _approve(account, _msgSender(), decreasedAllowance);
-        _burn(account, amount);
-
-        _moveDelegates(_delegates[account], address(0), amount);
+    function toggleFee(bool _overallFeeStatus, bool _isSenderTaxed, bool _isReceiverTaxed) external onlyOwner {
+        feeOn = _overallFeeStatus;
+        isSenderTaxed = _isSenderTaxed;
+        isReceiverTaxed = _isReceiverTaxed;
     }
 
-    /**
-     * @dev See {IERC20-transfer}.
-     *
-     * Requirements:
-     *
-     * - `recipient` cannot be the zero address.
-     * - the caller must have a balance of at least `amount`.
-     */
-    function transfer(address recipient, uint256 amount) public virtual override whenNotPaused returns (bool) {
-        address sender = _msgSender();
-        _transfer(sender, recipient, amount);
-        _moveDelegates(_delegates[sender], _delegates[recipient], amount);
-        return true;
+    function updateDexAddress(address _dex, bool _status) external onlyOwner {
+        isDex[_dex] = _status;
+
+        emit UpdateDex(_dex, _status);
     }
 
-    /**
-     * @dev See {IERC20-approve}.
-     *
-     * Requirements:
-     *
-     * - `spender` cannot be the zero address.
-     */
-    function approve(address spender, uint256 amount) public virtual override whenNotPaused returns (bool) {
-        super.approve(spender, amount);
+    function balanceOf(address account) public view override returns (uint256) {
+        return _balances[account];
     }
 
-    /**
-     * @dev See {IERC20-transferFrom}.
-     *
-     * Emits an {Approval} event indicating the updated allowance. This is not
-     * required by the EIP. See the note at the beginning of {ERC20}.
-     *
-     * Requirements:
-     *
-     * - `sender` and `recipient` cannot be the zero address.
-     * - `sender` must have a balance of at least `amount`.
-     * - the caller must have allowance for ``sender``'s tokens of at least
-     * `amount`.
-     */
-    function transferFrom(address sender, address recipient, uint256 amount) public virtual override whenNotPaused returns (bool) {
-        address spender = _msgSender();
-        uint256 spenderAllowance = allowance(sender, spender);
-        if (spenderAllowance != type(uint256).max) {
-            _approve(sender, spender, spenderAllowance.sub(amount, "ERC20: transfer amount exceeds allowance"));
-        }
-
-        _transfer(sender, recipient, amount);
-        _moveDelegates(_delegates[sender], _delegates[recipient], amount);
-        return true;
-    }
-
-    /**
-     * @dev Atomically increases the allowance granted to `spender` by the caller.
-     *
-     * This is an alternative to {approve} that can be used as a mitigation for
-     * problems described in {IERC20-approve}.
-     *
-     * Emits an {Approval} event indicating the updated allowance.
-     *
-     * Requirements:
-     *
-     * - `spender` cannot be the zero address.
-     */
-    function increaseAllowance(address spender, uint256 addedValue) public virtual override whenNotPaused returns (bool) {
-        super.increaseAllowance(spender, addedValue);
-    }
-
-    /**
-     * @dev Atomically decreases the allowance granted to `spender` by the caller.
-     *
-     * This is an alternative to {approve} that can be used as a mitigation for
-     * problems described in {IERC20-approve}.
-     *
-     * Emits an {Approval} event indicating the updated allowance.
-     *
-     * Requirements:
-     *
-     * - `spender` cannot be the zero address.
-     * - `spender` must have allowance for the caller of at least
-     * `subtractedValue`.
-     */
-    function decreaseAllowance(address spender, uint256 subtractedValue) public virtual override whenNotPaused returns (bool) {
-        super.increaseAllowance(spender, subtractedValue);
-    }
-
-    /**
-     * @notice Get `delegatee` of `delegator`
-     * @param delegator The address to get delegatee for
-     */
-    function delegates(address delegator)
-        external
-        view
-        returns (address)
-    {
-        return _delegates[delegator];
-    }
-
-   /**
-    * @notice Delegate votes from `msg.sender` to `delegatee`
-    * @param delegatee The address to delegate votes to
-    */
-    function delegate(address delegatee) external whenNotPaused {
-        return _delegate(msg.sender, delegatee);
-    }
-
-    /**
-     * @notice Delegates votes from signatory to `delegatee`
-     * @param delegatee The address to delegate votes to
-     * @param nonce The contract state required to match the signature
-     * @param expiry The time at which to expire the signature
-     * @param v The recovery byte of the signature
-     * @param r Half of the ECDSA signature pair
-     * @param s Half of the ECDSA signature pair
-     */
-    function delegateBySig(
-        address delegatee,
-        uint nonce,
-        uint expiry,
-        uint8 v,
-        bytes32 r,
-        bytes32 s
-    )
-        external
-        whenNotPaused
-    {
-        bytes32 domainSeparator = keccak256(
-            abi.encode(
-                DOMAIN_TYPEHASH,
-                keccak256(bytes(name())),
-                getChainId(),
-                address(this)
-            )
-        );
-
-        bytes32 structHash = keccak256(
-            abi.encode(
-                DELEGATION_TYPEHASH,
-                delegatee,
-                nonce,
-                expiry
-            )
-        );
-
-        bytes32 digest = keccak256(
-            abi.encodePacked(
-                "\x19\x01",
-                domainSeparator,
-                structHash
-            )
-        );
-
-        address signatory = ecrecover(digest, v, r, s);
-        require(signatory != address(0), "delegateBySig: invalid signature");
-        require(nonce == nonces[signatory]++, "delegateBySig: invalid nonce");
-        require(block.timestamp <= expiry, "delegateBySig: signature expired");
-        return _delegate(signatory, delegatee);
-    }
-
-    /**
-     * @notice Gets the current votes balance for `account`
-     * @param account The address to get votes balance
-     * @return The number of current votes for `account`
-     */
-    function getCurrentVotes(address account)
-        external
-        view
-        returns (uint256)
-    {
-        uint32 nCheckpoints = numCheckpoints[account];
-        return nCheckpoints > 0 ? checkpoints[account][nCheckpoints - 1].votes : 0;
-    }
-
-    /**
-     * @notice Determine the prior number of votes for an account as of a block number
-     * @dev Block number must be a finalized block or else this function will revert to prevent misinformation.
-     * @param account The address of the account to check
-     * @param blockNumber The block number to get the vote balance at
-     * @return The number of votes the account had as of the given block
-     */
-    function getPriorVotes(address account, uint blockNumber)
-        external
-        view
-        returns (uint256)
-    {
-        require(blockNumber < block.number, "getPriorVotes: not yet determined");
-
-        uint32 nCheckpoints = numCheckpoints[account];
-        if (nCheckpoints == 0) {
-            return 0;
-        }
-
-        // First check most recent balance
-        if (checkpoints[account][nCheckpoints - 1].fromBlock <= blockNumber) {
-            return checkpoints[account][nCheckpoints - 1].votes;
-        }
-
-        // Next check implicit zero balance
-        if (checkpoints[account][0].fromBlock > blockNumber) {
-            return 0;
-        }
-
-        uint32 lower = 0;
-        uint32 upper = nCheckpoints - 1;
-        while (upper > lower) {
-            uint32 center = upper - (upper - lower) / 2; // ceil, avoiding overflow
-            Checkpoint memory cp = checkpoints[account][center];
-            if (cp.fromBlock == blockNumber) {
-                return cp.votes;
-            } else if (cp.fromBlock < blockNumber) {
-                lower = center;
-            } else {
-                upper = center - 1;
-            }
-        }
-        return checkpoints[account][lower].votes;
-    }
-
-    function _delegate(address delegator, address delegatee)
-        internal
-    {
-        address currentDelegate = _delegates[delegator];
-        uint256 delegatorBalance = balanceOf(delegator); // balance of underlying tokens (not scaled)
-        _delegates[delegator] = delegatee;
-
-        emit DelegateChanged(delegator, currentDelegate, delegatee);
-
-        _moveDelegates(currentDelegate, delegatee, delegatorBalance);
-    }
-
-    function _moveDelegates(address srcRep, address dstRep, uint256 amount) internal {
-        if (srcRep != dstRep && amount > 0) {
-            if (srcRep != address(0)) {
-                // decrease old representative
-                uint32 srcRepNum = numCheckpoints[srcRep];
-                uint256 srcRepOld = srcRepNum > 0 ? checkpoints[srcRep][srcRepNum - 1].votes : 0;
-                uint256 srcRepNew;
-                if (srcRepOld > amount) {
-                    srcRepNew = srcRepOld.sub(amount);
-                } else {
-                    srcRepNew = 0;
-                    amount = srcRepOld;
-                }
-                _writeCheckpoint(srcRep, srcRepNum, srcRepOld, srcRepNew);
-            }
-
-            if (dstRep != address(0)) {
-                // increase new representative
-                uint32 dstRepNum = numCheckpoints[dstRep];
-                uint256 dstRepOld = dstRepNum > 0 ? checkpoints[dstRep][dstRepNum - 1].votes : 0;
-                uint256 dstRepNew = dstRepOld.add(amount);
-                _writeCheckpoint(dstRep, dstRepNum, dstRepOld, dstRepNew);
-            }
-        }
-    }
-
-    function _writeCheckpoint(
-        address delegatee,
-        uint32 nCheckpoints,
-        uint256 oldVotes,
-        uint256 newVotes
-    )
-        internal
-    {
-        uint32 blockNumber = safe32(block.number, "_writeCheckpoint: block number exceeds 32 bits");
-
-        if (nCheckpoints > 0 && checkpoints[delegatee][nCheckpoints - 1].fromBlock == blockNumber) {
-            checkpoints[delegatee][nCheckpoints - 1].votes = newVotes;
-        } else {
-            checkpoints[delegatee][nCheckpoints] = Checkpoint(blockNumber, newVotes);
-            numCheckpoints[delegatee] = nCheckpoints + 1;
-        }
-
-        emit DelegateVotesChanged(delegatee, oldVotes, newVotes);
-    }
-
-    function safe32(uint n, string memory errorMessage) internal pure returns (uint32) {
-        require(n < 2**32, errorMessage);
-        return uint32(n);
-    }
-
-    function getChainId() internal pure returns (uint) {
-        uint256 chainId;
-        assembly { chainId := chainid() }
-        return chainId;
+    function totalSupply() public view virtual override returns (uint256) {
+        return _totalSupply;
     }
 
     function _beforeTokenTransfer(address from, address to, uint256 amount) 
         internal 
         virtual 
-        override(ERC20Upgradeable, ERC20PausableUpgradeable, ERC20SnapshotUpgradeable) 
+        override (ERC20Upgradeable) 
     {
         super._beforeTokenTransfer(from, to, amount);
     }
 
-    /**
-     * @dev called by the owner to pause, triggers stopped state
-     */
-    function pause() public onlyOwner whenNotPaused {
-        _pause();
+    function mint(address _to, uint256 _amount) public onlyTreasury {
+        _mint(_to, _amount);
     }
 
-    /**
-     * @dev called by the owner to unpause, returns to normal state
-     */
-    function unpause() public onlyOwner whenPaused {
-        _unpause();
+    function _mint(address account, uint amount) internal override {
+        require(account != address(0), "ERC20: mint to the zero address");
+
+        _beforeTokenTransfer(address(0), account, amount);
+
+        _totalSupply += amount;
+        _balances[account] += amount;
+        emit Transfer(address(0), account, amount);
     }
 
-    /** 
-     * Creates a new snapshot id. Balances are only stored in snapshots on demand: unless a snapshot was taken, a
-     * balance change will not be recorded. This means the extra added cost of storing snapshotted balances is only paid
-     * when required, but is also flexible enough that it allows for e.g. daily snapshots.
-     */ 
-    function snapshot() public returns (uint256) {
-        _snapshot();
-    }
+    function _burn(address account, uint amount) internal override {
+        require(account != address(0), "ERC20: burn from the zero address");
 
-    uint256[46] private __gap;
+        _beforeTokenTransfer(account, address(0), amount);
+
+        uint256 accountBalance = _balances[account];
+        require(accountBalance >= amount, "ERC20: burn amount exceeds balance");
+        unchecked {
+            _balances[account] = accountBalance - amount;
+        }
+        _totalSupply -= amount;
+
+        emit Transfer(account, address(0), amount);
+    }
     
+    function _transfer(address sender, address recipient, uint amount) internal override {
+        require(sender != address(0), "ERC20: transfer from the zero address");
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+
+        _beforeTokenTransfer(sender, recipient, amount);
+
+        
+        uint _treasuryTax = isDex[sender] || isDex[recipient] ? amount * taxPerc / 10000 : 0;
+
+        uint _amountSender = amount;
+        uint _amountReceiver = amount;
+        uint _totalTax;
+
+        if(feeOn && (isDex[sender] || isDex[recipient])) {
+            if(isSenderTaxed) {
+                _amountSender = _amountSender + _treasuryTax;
+                _totalTax = _totalTax + _treasuryTax ;
+            }
+
+            if(isReceiverTaxed) {
+                _amountReceiver = _amountReceiver - _treasuryTax;
+                _totalTax = _totalTax + _treasuryTax;
+
+            }
+        }
+        require(_balances[sender] >= _amountSender, "ERC20: transfer amount exceeds balance");
+
+        unchecked {
+            _balances[sender] = _balances[sender] - _amountSender;
+            _balances[taxReceiver] = _balances[taxReceiver] + (_totalTax);
+            _balances[recipient] = _balances[recipient] + (_amountReceiver);
+        }
+
+        emit Transfer(sender, recipient, amount);
+    }
+
 }
