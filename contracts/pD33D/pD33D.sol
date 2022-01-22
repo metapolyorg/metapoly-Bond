7bbf152fa59c415fbf27f695d6fa29bc72b0b012
Siew <siewchongche@gmail.com>
2022-01-23 02:11:05 +0800

    Revamp pD33D & pD33DRedeemer

--- contracts/pD33D/pD33D.sol
+++ contracts/pD33D/pD33D.sol
@@ -1,26 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.7;
 
-// import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20CappedUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-contract pD33DImplementation is Initializable, ERC20CappedUpgradeable, ERC20BurnableUpgradeable, OwnableUpgradeable {
+contract pD33D is Initializable, OwnableUpgradeable, ERC20BurnableUpgradeable {
 
-    function initalize(uint _cap) external initializer {
+    address public pD33DRedeemer;
+
+    function initialize(uint _supply) external initializer {
         __ERC20_init("Pre-D33D", "pD33D");
-        __ERC20Capped_init(_cap);
         __Ownable_init();
+
+        _mint(msg.sender, _supply);
     }
 
-    function _mint(address account, uint256 amount) internal override(ERC20Upgradeable, ERC20CappedUpgradeable) {
-        super._mint(account, amount);
+    function burnFrom(address account, uint256 amount) public override {
+        require(msg.sender == pD33DRedeemer, "Only redeemer");
+
+        _burn(account, amount);
     }
 
-    function mint(address account, uint256 amount) external onlyOwner {
-        _mint(account, amount);
+    function setRedeemer(address _pD33DRedeemer) external onlyOwner {
+        pD33DRedeemer = _pD33DRedeemer;
     }
-
 }
--- contracts/pD33D/pD33DRedeemer.sol
+++ contracts/pD33D/pD33DRedeemer.sol
@@ -4,24 +4,29 @@ import "@openzeppelin/contracts-upgradeable/interfaces/IERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/cryptography/ECDSAUpgradeable.sol";
 
 interface ITreasury {
     function deposit(uint _amount, address _token, uint _profit) external returns (uint);
     function d33dPrice() external view returns (uint);
 }
 
+interface IStakingContract {
+    function stake(uint _amount, address _receiver) external;
+}
+
 interface IERC20 is IERC20Upgradeable {
-    function decimals() external view returns (uint);
     function burnFrom(address, uint) external;
 }
 
-contract pD33DRedeemerImplementation is Initializable, OwnableUpgradeable {
-
-    using SafeERC20Upgradeable for IERC20;
+contract pD33DRedeemer is Initializable, OwnableUpgradeable {
 
     IERC20 public D33D;
     IERC20 public pD33D;
+    IERC20 constant USDC = IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
     ITreasury public treasury;
+    IStakingContract public stakingContract;
+    address public signer;
 
     struct Term {
         uint percent; // 4 decimals ( 5000 = 0.5% )
@@ -31,46 +36,49 @@ contract pD33DRedeemerImplementation is Initializable, OwnableUpgradeable {
 
     mapping( address => Term ) public terms;
     
-    mapping( address => address ) public walletChange;
-    
-    function initialize(IERC20 _D33D, IERC20 _pD33D, ITreasury _treasury) external initializer {
+    function initialize(IERC20 _D33D, IERC20 _pD33D, address _signer) external initializer {
         D33D = _D33D;
         pD33D = _pD33D;
-        treasury = _treasury;
+        signer = _signer;
 
         __Ownable_init();
     }
 
     ///@param _vester user address
     ///@param _amountCanClaim allocated pTOKEN
-    ///@param _rate supplyshare 2 decimals (500 for 5% supplyshare)
-    function setTerms(address _vester, uint _amountCanClaim, uint _rate ) external onlyOwner returns ( bool ) {
+    ///@param _rate supply share 2 decimals (500 for 5% supplyshare)
+    function setTerms(address _vester, uint _amountCanClaim, uint _rate ) external onlyOwner {
         require( _amountCanClaim >= terms[ _vester ].max, "cannot lower amount claimable" );
-        require( _rate >= terms[ _vester ].percent, "cannot lower vesting rate" );
 
         terms[ _vester ].max = _amountCanClaim;
         terms[ _vester ].percent = _rate;
-
-        return true;
     }
 
-    function exercise( uint _amount, address _token ) external returns ( bool ) {
-        Term memory info = terms[ msg.sender ];
-        require( redeemable( info ) >= _amount, 'Not enough vested' );
-        require( info.max -  info.claimed >= _amount, 'Claimed over max' );
+    function redeem( uint _amount, bytes calldata _signature, bool _stake ) external {
+        if (_signature.length == 0) { // No signature, whitelisted in contract
+            Term memory info = terms[ msg.sender ];
+            require( redeemable( info ) >= _amount, 'Not enough vested' );
+            require( info.max -  info.claimed >= _amount, 'Claimed over max' );
 
-        uint principleAmount = (_amount * treasury.d33dPrice() / 1e18) * IERC20( _token ).decimals() / 1e18;
-        IERC20( _token ).safeTransferFrom( msg.sender, address( this ), principleAmount );
+            terms[ msg.sender ].claimed = info.claimed +  _amount;
+        } else {
+            bytes32 message = keccak256(abi.encodePacked(msg.sender));
+            bytes32 messageHash = ECDSAUpgradeable.toEthSignedMessageHash(message);
+            address recoveredAddr = ECDSAUpgradeable.recover(messageHash, _signature);
+            require(recoveredAddr == signer, "Invalid signature");
+        }
+
+        uint USDCAmt = (_amount * treasury.d33dPrice() / 1e30);
+        USDC.transferFrom( msg.sender, address( this ), USDCAmt );
         pD33D.burnFrom( msg.sender, _amount );
         
-        IERC20( _token ).approve( address(treasury), principleAmount );
-        uint amt = treasury.deposit( principleAmount, _token, 0);
+        uint D33DAmt = treasury.deposit( USDCAmt, address(USDC), 0);
 
-        terms[ msg.sender ].claimed = info.claimed +  _amount;
-
-        D33D.safeTransfer( msg.sender, amt );
-
-        return true;
+        if (_stake) {
+            stakingContract.stake(D33DAmt, msg.sender);
+        } else {
+            D33D.transfer( msg.sender, D33DAmt );
+        }
     }
 
     function redeemableFor( address _vester ) public view returns (uint) {
@@ -81,22 +89,28 @@ contract pD33DRedeemerImplementation is Initializable, OwnableUpgradeable {
         return ( D33D.totalSupply() *_info.percent / 10000 ) - ( _info.claimed );
     }
 
-    // Allows wallet owner to transfer rights to a new address
-    function pushWalletChange( address _newWallet ) external returns ( bool ) {
+    function changeWhitelistedAddress( address _newWallet ) external {
         require( terms[ msg.sender ].percent != 0 );
-        walletChange[ msg.sender ] = _newWallet;
-        return true;
+        terms[ _newWallet ] = terms[ msg.sender ];
+        delete terms[ msg.sender ];
     }
-    
-    // Allows wallet to pull rights from an old address
-    function pullWalletChange( address _oldWallet ) external returns ( bool ) {
-        require( walletChange[ _oldWallet ] == msg.sender, "wallet did not push" );
-        
-        walletChange[ _oldWallet ] = address(0);
-        terms[ msg.sender ] = terms[ _oldWallet ];
-        delete terms[ _oldWallet ];
-        
-        return true;
+
+    function setTreasury(ITreasury _treasury) external onlyOwner {
+        treasury = _treasury;
+        USDC.approve(address(_treasury), type(uint).max);
     }
-    
+
+    function setStakingContract(IStakingContract _stakingContract) external onlyOwner {
+        stakingContract = _stakingContract;
+        D33D.approve(address(stakingContract), type(uint).max);
+    }
+
+    function setSigner(address _signer) external onlyOwner {
+        signer = _signer;
+    }
+
+    function isContractWhitelisted(address _account) external view returns (bool) {
+        if (terms[ _account ].percent != 0) return true;
+        else return false;
+    }
 }
